b175805d2d24ca59f28e7a0973ce911a711443b8
Slava Chalnev <slava.chalnev@gmail.com>
2023-03-24 15:10:11 +1100

    Train replacement mlp

---
+++ solu_lens/graft.py
@@ -0,0 +1,93 @@
+import torch
+from torch import nn
+from torch.utils.tensorboard import SummaryWriter
+# from torch.utils.data import DataLoader
+# from datasets import load_dataset
+import transformer_lens
+from transformer_lens import HookedTransformer
+from transformer_lens import utils
+
+from mlp import SoluMLP, GeluMLP
+
+
+
+d_model = 768
+solu_layer = SoluMLP(input_size=d_model, hidden_size=d_model*4, output_size=d_model)
+gelu_layer = GeluMLP(input_size=d_model, hidden_size=d_model*4, output_size=d_model)
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+gpt2 = HookedTransformer.from_pretrained("gpt2-small", device=device)
+tokenizer = gpt2.tokenizer
+
+loader = transformer_lens.evals.make_pile_data_loader(tokenizer=tokenizer, batch_size=8)
+
+
+pre = None
+post = None
+
+def mlp_pre(value, hook):
+    global pre
+    pre = value.detach().clone().cpu()
+    return value
+
+def mlp_post(value, hook):
+    global post
+    post = value.detach().clone().cpu()
+    return value
+
+layer_to_hook = 3
+
+fwd_hooks = [
+    (utils.get_act_name("attn_out", layer_to_hook), mlp_pre),
+    (utils.get_act_name("mlp_out", layer_to_hook), mlp_post)
+]
+
+solu_layer.to(device)
+gelu_layer.to(device)
+solu_layer.train()
+gelu_layer.train()
+
+criterion = nn.MSELoss()
+s_optimizer = torch.optim.Adam(solu_layer.parameters(), lr=1e-4)
+g_optimizer = torch.optim.Adam(gelu_layer.parameters(), lr=1e-4)
+
+writer = SummaryWriter()
+
+for epoch in range(10):
+    s_loss_total = 0
+    g_loss_total = 0
+    for idx, batch in enumerate(loader):
+        # print("Batch")
+        # print(batch)
+        with torch.no_grad():
+            loss = gpt2.run_with_hooks(
+                batch["tokens"].to(device), 
+                return_type="loss", 
+                fwd_hooks=fwd_hooks,
+            )
+        s_optimizer.zero_grad()
+        g_optimizer.zero_grad()
+
+        s_loss = criterion(solu_layer(pre.to(device)), post.to(device))
+        g_loss = criterion(gelu_layer(pre.to(device)), post.to(device))
+
+        s_loss.backward()
+        g_loss.backward()
+
+        s_optimizer.step()
+        g_optimizer.step()
+
+        s_loss_total += s_loss.item()
+        g_loss_total += g_loss.item()
+
+        if idx % 10 == 0:
+            # log to tensorboard
+            writer.add_scalar("Loss/S", s_loss.item(), epoch*len(loader) + idx)
+            writer.add_scalar("Loss/G", g_loss.item(), epoch*len(loader) + idx)
+
+
+        print(f"Epoch: {epoch} step {idx}, S Loss: {s_loss}, G Loss: {g_loss}")
+    print(f"end Epoch: {epoch}, S Loss: {s_loss_total/len(loader)}, G Loss: {g_loss_total/len(loader)}")
+
--- solu_lens/mlp.py
+++ solu_lens/mlp.py
@@ -24,4 +24,20 @@ class SoluMLP(nn.Module):
         x = self.layer_norm(x)
         x = self.fc2(x)
         return x
+
+class GeluMLP(nn.Module):
+    def __init__(self, input_size: int, hidden_size: int, output_size: int):
+        super().__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, output_size)
+        self.activation = nn.GELU()
+    
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.activation(x)
+        x = self.fc2(x)
+        return x
     
