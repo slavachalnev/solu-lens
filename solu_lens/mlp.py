82e95d0f4445152e4af97d6ba7a32f80afa39f95
Slava Chalnev <slava.chalnev@gmail.com>
2023-03-24 13:39:31 +1100

    Add SoLU

---
+++ solu_lens/mlp.py
@@ -0,0 +1,27 @@
+import torch
+from torch import nn
+
+
+class SoLU(nn.Module):
+    def forward(self, x):
+        return x * torch.softmax(x, dim=-1)
+
+
+class SoluMLP(nn.Module):
+    def __init__(self, input_size: int, hidden_size: int, output_size: int):
+        super().__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, output_size)
+        self.layer_norm = nn.LayerNorm(hidden_size)
+        self.activation = SoLU()
+    
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.activation(x)
+        x = self.layer_norm(x)
+        x = self.fc2(x)
+        return x
+    
